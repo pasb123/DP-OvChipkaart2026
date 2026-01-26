f213233b79c8381bac63f51e9a066b3310c86fde
pasb123 <pasquinel2space@gmail.com>
2026-01-26 14:35:46 +0100

    p1

---
+++ src/main/java/nl/hu/dp/dao/ReizigerDAOPsql.java
@@ -0,0 +1,108 @@
+package nl.hu.dp.dao;
+
+import nl.hu.dp.domain.Reiziger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReizigerDAOPsql implements ReizigerDAO{
+    private Connection conn;
+    public ReizigerDAOPsql(Connection conn) {
+        this.conn = conn;
+    }
+    @Override
+    public boolean save(Reiziger reiziger) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement("" +
+                "INSERT INTO reiziger (reiziger_id, voorletters, tussenvoegsel,achternaam,geboortedatum) VALUES (?, ?, ?,?,?)");
+        statement.setInt(1, reiziger.getId());
+        statement.setString(2, reiziger.getVoorletters());
+        statement.setString(3, reiziger.getTussenvoegsel());
+        statement.setString(4, reiziger.getAchternaam());
+        statement.setDate(5,reiziger.getGeboortedatum());
+        statement.execute();
+        statement.close();
+        return true;
+    }
+
+    @Override
+    public boolean update(Reiziger reiziger) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement("" + "update reiziger set voorletters=?, tussenvoegsel=?, achternaam=?,geboortedatum=? Where reiziger_id=?");
+        statement.setString(1, reiziger.getVoorletters());
+        statement.setString(2, reiziger.getTussenvoegsel());
+        statement.setString(3, reiziger.getAchternaam());
+        statement.setDate(4, reiziger.getGeboortedatum());
+        statement.setInt(5, reiziger.getId());
+        statement.executeUpdate();
+        statement.close();
+
+        return false;
+    }
+
+    @Override
+    public boolean delete(Reiziger reiziger) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement("" +"DELETE FROM reiziger where reiziger_id=?");
+        statement.setInt(1, reiziger.getId());
+        statement.execute();
+        statement.close();
+        return false;
+    }
+
+    @Override
+    public Reiziger findById(int id) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM reiziger WHERE reiziger_id = ?");
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+
+        Reiziger reiziger = null;
+        if (resultSet.next()) {
+            reiziger = new Reiziger(
+                    resultSet.getInt("reiziger_id"),
+                    resultSet.getString("voorletters"),
+                    resultSet.getString("tussenvoegsel"),
+                    resultSet.getString("achternaam"),
+                    resultSet.getDate("geboortedatum")
+            );
+        }
+
+        resultSet.close();
+        statement.close();
+
+        return reiziger;
+    }
+
+    @Override
+    public List<Reiziger> findByGbdatum(Date date) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM reiziger WHERE geboortedatum = ?");
+        statement.setDate(1, new java.sql.Date(date.getTime()));
+        ResultSet resultSet = statement.executeQuery();
+        List<Reiziger>Reizigerlist = new ArrayList();
+        while (resultSet.next()) {
+            Reiziger reiziger= new Reiziger(
+                    resultSet.getInt("reiziger_id"),
+                    resultSet.getString("voorletters"),
+                    resultSet.getString("tussenvoegsel"),
+                    resultSet.getString("achternaam"),
+                    resultSet.getDate("geboortedatum")
+            );
+            Reizigerlist.add(reiziger);
+        }
+        resultSet.close();
+        statement.close();
+        return Reizigerlist;
+
+    }
+
+    @Override
+    public List<Reiziger> findAll() throws SQLException {
+        ResultSet resultSet =conn.createStatement().executeQuery("SELECT * FROM reiziger");
+        List<Reiziger>Reizigerlist = new ArrayList();
+        while (resultSet.next()){
+            Reiziger reiziger= new  Reiziger(resultSet.getInt("reiziger_id"),resultSet.getString("voorletters"),resultSet.getString("tussenvoegsel"),
+                    resultSet.getString("achternaam"),resultSet.getDate("geboortedatum"));
+
+            Reizigerlist.add(reiziger);
+        }
+        return Reizigerlist;
+    }
+}
